9123a0455807e35224f1f8b0324bc03c88ead952
Piyush Bhardwaj <piyush.bhardwaj1@ibm.com>
2024-04-15 09:41:09 +0530

    Temperature Project Initialized

---
+++ temperature.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class temperature
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the temperature in Kelvin");
+		int tempK= sc.nextInt();
+		int tempC=tempK - 273;
+		System.out.print("The temperature in Celcius is: ");
+	    System.out.println(tempC);
+	}
+}
